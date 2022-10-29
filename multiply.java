45f756d8a3e7556f81309203cda270028ad25793
minu <minukairali@gmail.com>
2022-10-29 14:53:58 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,12 @@
+ public class multiply {
+ 
+       Run | Debug
+        int a = 4;
+        int b = 2;
+        
+           system.out.println("the product is " + (a+b)
+            
+              } 
+              
+      }        
+             
